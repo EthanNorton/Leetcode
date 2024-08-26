b9d64f32e8c394b5719e0a0afa5a646c564e7956
Ethan <ethannorton12@gmail.com>
2024-08-26 14:16:43 -0500

    solved and defined some problems

---
+++ Queue/main.py
@@ -0,0 +1,14 @@
+## coming back to queue, a bit too hard right now 
+
+class RecentCounter:
+
+    def __init__(self):
+        
+
+    def ping(self, t: int) -> int:
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
