b12a748f807005d5bcf82c5ab0b2c59dd4cd1463
Ethan <ethannorton12@gmail.com>
2024-09-24 15:17:02 -0500

    daily leetcodes

--- Strings/Find the Difference/main.py
+++ Strings/Find the Difference/main.py
@@ -1,3 +1,6 @@
+# not familiar with this, will come back
+
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        
+        result = 0
+        for i in range(len())
---
+++ Strings/Find the Index of the First Occurrence in a String/main.py
@@ -0,0 +1,3 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle) # using the find function, and solving easy way 
