16072ee2923a0980993a083112bc1e6b582ac387
Ethan <ethannorton12@gmail.com>
2024-10-01 16:22:33 -0500

    daily leetcode

--- Arrays/Remove Element/main.py
+++ Arrays/Remove Element/main.py
@@ -1,5 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-
-# array manipulation 
-# two-pointer technique 
+        k = 0  
+        
+       
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1  
+        
+        return k
